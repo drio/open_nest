60a01a283735ec0924bb965daba39a2fcd6f3870
David Rio Deiros <driodeiros@gmail.com>
2015-08-30 11:28:24 -0400

    Adding RGB led and temperature measurements.

--- open_nest.ino
+++ open_nest.ino
@@ -2,6 +2,15 @@
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
+#define PIN_POT        A0
+#define PIN_TEMP       A1
+#define PIN_RED        9
+#define PIN_GREEN      10
+#define PIN_BLUE       11
+#define STATUS_AC_ON   0
+#define STATUS_HEAT_ON 1
+#define STATUS_OFF     2
+
 Adafruit_8x8matrix matrix_low = Adafruit_8x8matrix();
 Adafruit_8x8matrix matrix_high = Adafruit_8x8matrix();
 
@@ -22,27 +31,58 @@ void display_init() {
   matrix_high.begin(0x71);
 }
 
+float current_temp() {
+  float voltage, celsius, fahrenheit;
+  voltage    = (analogRead(PIN_TEMP) * 5.0) / 1024.0;
+  celsius    = (voltage - 0.5) * 100.0;
+  fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+  Serial.print("Volts: "); Serial.println(voltage);
+  Serial.print("Celsius: "); Serial.println(celsius);
+  Serial.print("fahrenheit: "); Serial.println(fahrenheit);
+  Serial.println("");
+  return celsius;
+}
+
+void led_init() {
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+}
+
+void led_color(int r, int g, int b) {
+  analogWrite(PIN_RED, r);
+  analogWrite(PIN_GREEN, g);
+  analogWrite(PIN_BLUE, b);
+}
+
+void change_status(int status) {
+  if (status == STATUS_AC_ON)   led_color(0,0,255);
+  if (status == STATUS_HEAT_ON) led_color(255,0,0);
+  if (status == STATUS_OFF)     led_color(0,0,0);
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
 
+  led_init();
   display_init();
   display_number("39");
 }
 
 void loop() {
-
   int voltage = analogRead(A0) * (5.0 / 1023.0);
-
   char buf[12];
   itoa(voltage, buf, 10);
-  display_number();
+  display_number(buf);
 
-  Serial.println(analogRead(A0));
-  Serial.println(voltage);
-  Serial.println(buf);
-  Serial.println("--");
+  current_temp();
 
+  change_status(STATUS_AC_ON);
   delay(2000);
-  //display.doIt();
+  change_status(STATUS_HEAT_ON);
+  delay(2000);
+  change_status(STATUS_OFF);
+  delay(2000);
 }
