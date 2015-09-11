b9e65e54dae813e8f52371c31b2d7cf93147bdd4
David Rio Deiros <driodeiros@gmail.com>
2015-09-10 23:12:49 -0400

    Use radio to listen for temperature (very basic).

--- src/open_nest/open_nest.ino
+++ src/open_nest/open_nest.ino
@@ -1,5 +1,10 @@
 #include <Wire.h>
 #include <Arduino.h>
+#include <SPI.h>
+
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
 
 #include "led.h"
 #include "display.h"
@@ -14,16 +19,51 @@ int lastChantedPot = -80;
 int desiredTemp = 0;
 int currentTemp = 0;
 
+RF24 radio(9,10);
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   led_init();
   display_init();
   temp_init();
   relay_init();
+
+  Serial.begin(57600);
+  printf_begin();
+  printf("\nRemote Switch Arduino\n\r");
+
+  radio.begin();
+  radio.setRetries(15,15);
+
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.startListening();
+  radio.printDetails();
 }
 
 void loop() {
   static int wait = 0;
+
+  if (radio.available()) {
+    printf("Radio ... %d\n", radio.available());
+    unsigned short rawMessage;
+    radio.read( &rawMessage, sizeof(unsigned long) );
+    printf("Got message %d...",rawMessage);
+
+    //performAction(rawMessage);
+    desiredTemp = (int) rawMessage;
+    display_number(desiredTemp);
+
+    //send back
+    radio.stopListening();
+    unsigned short answer = 27;
+    radio.write(&answer, sizeof(unsigned short) );
+    printf("Sent response.\n\r");
+    radio.startListening();
+
+    delay(10);
+  }
 
   if (!wait)  { // sample current temperature
     wait = 1000;
